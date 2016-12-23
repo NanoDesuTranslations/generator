937e4cdb3ba39bd7c4e9d106b09e09d82d786e09
Yarn <yarnnd@gmail.com>
2016-12-22 20:23:29 -0800

    fix ordering of blog posts, show publish date and pinned

--- loader/blog.js
+++ loader/blog.js
@@ -19,6 +19,14 @@ function groupBy(array, predicate){
   return d;
 }
 
+function format_unix_time(unix_time){
+  var t = new Date(unix_time * 1000);
+  var formatted = t.toISOString();
+  formatted = formatted.replace("T0", "T");
+  formatted = formatted.replace("T", " ").replace(":00.000Z", "");
+  return formatted;
+}
+
 function sorted_post_keys(posts){
   var keys = Object.keys(posts);
   keys = keys.sort(function(a, b){
@@ -29,9 +37,9 @@ function sorted_post_keys(posts){
     }else if(!a.meta.blog.pinned && b.meta.blog.pinned){
       return 1;
     }
-    if(a.meta.blog.publish_date < b.meta.blog.publish_date){
+    if(a.meta.blog.published_date > b.meta.blog.published_date){
       return -1;
-    }else if(a.meta.blog.publish_date > b.meta.blog.publish_date){
+    }else if(a.meta.blog.published_date < b.meta.blog.published_date){
       return 1;
     }
     return 0;
@@ -52,6 +60,11 @@ function addBlog(tree, posts){
     for(var post_i of sorted_post_keys(s_posts)){
       post = s_posts[post_i];
       s_content += "<h2>" + post.meta.title + "</h2>";
+      s_content += "<h6>" + format_unix_time(post.meta.blog.published_date);
+      if(post.meta.blog.pinned){
+        s_content += " [pinned]";
+      }
+      s_content += "</h6>";
       s_content += "<p>" + post.content + "</p><hr style='border-color:white' />";
     }
     tree[serie].index = {
